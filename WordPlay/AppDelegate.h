37731d1241db6ff2bccdcd1f43061d0ff8d3824d
Matt Larkin <mattlarkin@me.com>
2015-03-10 15:06:03 -0400

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Matt Larkin on 3/10/15.
+//  Copyright (c) 2015 Matt Larkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
